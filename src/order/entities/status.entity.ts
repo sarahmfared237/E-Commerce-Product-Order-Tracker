83cec378df24433dde1141d56809a0a3019034f2
sarah1234-lab <es-SarahMohamed2025@alexu.edu.eg>
2025-08-18 18:52:50 +0300

    fix nextStatusID type

--- src/order/entities/status.entity.ts
+++ src/order/entities/status.entity.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Document, Types } from 'mongoose';
 
 export type StatusDocument = Status & Document;
 
@@ -7,8 +7,9 @@ export type StatusDocument = Status & Document;
 export class Status {
   @Prop({ required: true, unique: true })
   name: string; 
-  @Prop({ required: true })
-  nextStatusID: string | null; 
+
+  @Prop({ type: Types.ObjectId, ref: 'Status', default: null })
+  nextStatusID: Types.ObjectId | null;
 }
 
 export const StatusSchema = SchemaFactory.createForClass(Status);
